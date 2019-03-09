2610dd49e6ada814b63b4fa602ea537510b61c37
liaochong <liaochong@guahao.com>
2019-03-09 17:57:32 +0800

    新增校验异常

---
+++ my-convention/my-convention-common/src/main/java/com/github/liaochong/myconvention/common/exception/ServiceInvalidException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 liaochong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.liaochong.myconvention.common.exception;
+
+import java.util.List;
+
+/**
+ * 框架使用，建议不要使用
+ *
+ * @author liaochong
+ * @version 1.0
+ */
+public class ServiceInvalidException extends RuntimeException {
+    private static final long serialVersionUID = 6595542294997611244L;
+
+    private List<String> violationItems;
+
+    public ServiceInvalidException(List<String> violationItems) {
+        this.violationItems = violationItems;
+    }
+
+    public List<String> getViolationItems() {
+        return violationItems;
+    }
+}

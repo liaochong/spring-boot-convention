48f329f09b3471505bda9a4d3870139bcc3030c0
liaochong <liaochong@guahao.com>
2019-03-09 14:24:33 +0800

    init message

---
+++ my-spring-boot-convention-example/src/main/java/com/github/liaochong/myspringbootconventionexample/MySpringBootConventionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.liaochong.myspringbootconventionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootConventionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootConventionExampleApplication.class, args);
+    }
+
+}

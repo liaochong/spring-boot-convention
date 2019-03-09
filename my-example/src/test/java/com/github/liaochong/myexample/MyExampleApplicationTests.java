c431736fa9bdfeea4c55cb329909ec4b8491275d
liaochong <liaochong@guahao.com>
2019-03-09 14:43:27 +0800

    init

--- my-example/src/test/java/com/github/liaochong/myexample/MyExampleApplicationTests.java
+++ my-example/src/test/java/com/github/liaochong/myexample/MyExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.liaochong.myspringbootconventionexample;
+package com.github.liaochong.myexample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MySpringBootConventionExampleApplicationTests {
+public class MyExampleApplicationTests {
 
     @Test
     public void contextLoads() {
